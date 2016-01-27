9822fdbfcc9febd7abc5a1b93f0b649abcd9dcd5
Lalit Pareek <lalit_pareek@yahoo.com>
2016-01-27 07:55:23 -0500

    Revert "testing"
    This reverts commit 585c9e646dd14b4c5d7656f326e31b06c0ba456f.

--- _site/NOTICE.txt
+++
@@ -1,1 +0,0 @@
-Copyright (c) 2016 SAP SE or an SAP affiliate company. All rights reserved.
--- _site/tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp dependency-mods/grunt-files-check/tasks/files_check.js node_modules/grunt-files-check/tasks/files_check.js
-cp dependency-mods/grunt-deadlink/tasks/logger.js node_modules/grunt-deadlink/tasks/logger.js
-grunt test -v
