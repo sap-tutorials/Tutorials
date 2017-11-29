f88ad6517ebbe14c268afc9877a68a7cd6e7d0b4
Anton Yaskevich <anton.yaskevich.mail@gmail.com>
2017-11-29 15:11:10 +0300

    fix(link-checker): increase req timeout, add trusted domains list, decode encoded urls

--- test_modules/checkDeadLink/checkDeadLink.js
+++ test_modules/checkDeadLink/checkDeadLink.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var checkLink = require('markdown-link-check');
-var request = require('request');
+
+var { domains } = require('./white-list');
 
 var options = {
   timeout: '60s',
@@ -9,9 +10,9 @@ var options = {
 
 module.exports = function(fname, links, callback) {
 
-    if(links.length == 0){
+    if(links.length == 0) {
       callback({"isPassed":true, "deadlinks":[]});
-    }else{
+    } else {
       //exclude readme files
       if (fname != "readme.md" && fname != "README.md") {
               var isPassed = true;
@@ -20,15 +21,21 @@ module.exports = function(fname, links, callback) {
           links.forEach(function(link){
             checkLink(link, options, function(err, results){
                var result = results[0];
-               if(result.status == 'dead'){
+               if(result.status == 'dead') {
                  isPassed = false;
+                 const isTrusted = !!domains.find(domain => link.includes(domain));
                  deadlinks.push({
-                   "url":result.link,
-                   "code":result.statusCode})
+                   "url": result.link,
+                   "code": result.statusCode,
+                   isTrusted
+                  });
                }
                index++;
                if(index == links.length){
-                 callback({"isPassed":isPassed,  "deadlinks":deadlinks});
+                 callback({
+                   "isPassed": isPassed,
+                   "deadlinks": deadlinks
+                  });
                }
             });
           })
