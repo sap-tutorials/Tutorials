cf56d48233f1885b2944d526e9e98cb69599ac49
Yevheniia_Rakhmatova <yevheniia_rakhmatova@epam.com>
2018-10-10 14:50:06 +0300

    Check node version before running test tool
    show warning message if user uses an outdated node version

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 var testTool = require('./test-tool/src');
 var checkNodeV = require('./check-node-v');
+
 var NODE_ENV = process.env.NODE_ENV;
 var isProduction = NODE_ENV == 'production';
 
-const { NODE_ENV } = process.env;
-const isProduction = NODE_ENV == 'production';
-
-testTool.run(__dirname, isProduction).then(result => {
-  if (result.passed) {
-    process.exit(0);
-  } else {
+checkNodeV().then(() => {
+  testTool.run(__dirname, isProduction).then(result => {
+    if (result.passed) {
+      process.exit(0);
+    } else {
+      process.exit(1);
+    }
+  }).catch((error) => {
+    console.log(error);
     process.exit(1);
-  }
-}).catch((error) => {
-  console.log(error);
-  process.exit(1);
+  });
 });
