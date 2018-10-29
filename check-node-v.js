234aa677c043c57afe8893c38d4ac586f2560bc0
Yevheniia_Rakhmatova <yevheniia_rakhmatova@epam.com>
2018-10-29 14:33:53 +0200

    DEVMS-1307: Check node v

--- check-node-v.js
+++ check-node-v.js
@@ -1,29 +1,35 @@
 'use strict';
 
 var childProcess = require('child_process');
-var util = require('util');
 var logger = require('color-log');
 var packageMeta = require('./package.json');
-
-var exec = util.promisify(childProcess.exec);
 
 module.exports = function () {
   logger.info('Checking node.js version...');
 
-  return exec('node -v')
-    .then(function(result) {
-      if (result.stderr) {
-        throw new Error(result.stderr);
+  return new Promise(function (resolve, reject) {
+    return childProcess.exec('node -v', function (error, stdout, stderr) {
+      if (error) {
+        return reject(error);
+      }
+      if (stderr) {
+        return reject(stderr);
       }
 
-      var minVersion = parseFloat(packageMeta.engines.node.replace(/[\=\<\>]*/g, ''));
-      var clearOut = (result.stdout).replace(/\s*/g, '');
-      var currentVersion = parseFloat((clearOut).replace('v', ''));
+
+      return resolve(stdout);
+    });
+  })
+    .then(function (result) {
+      var engine = packageMeta.engines.node.replace(/[\=\<\>]*/g, '');
+      var minVersion = parseFloat(engine);
+      var clearOut = result.replace(/\s*/g, '');
+      var currentVersion = parseFloat(clearOut.replace('v', ''));
 
       if (currentVersion < minVersion) {
         logger.error(`
           ATTENTION!
-          You are using an outdated Node.js version. Currently installed version ${clearOut} is not supported, please upgrade to v${parseInt(minVersion)} or higher.
+          You are using an outdated Node.js version. Currently installed version ${clearOut} is not supported, please upgrade to v${engine} or higher.
           Test tool cannot be run. Exiting.
           `);
         return process.exit(1);
@@ -31,8 +37,8 @@ module.exports = function () {
         logger.info(`Node version is ${clearOut}. Ready to run test tool.`);
       }
     })
-    .catch(function(error) {
-      logger.error(error);
+    .catch(function (error) {
       logger.error('Unable to check Node.js version. Trying to run test tool..');
+      logger.error(error);
     });
 };
--- test.js
+++ test.js
@@ -1,17 +1,18 @@
-var testTool = require('./test-tool/src');
 var checkNodeV = require('./check-node-v');
 
 var NODE_ENV = process.env.NODE_ENV;
 var isProduction = NODE_ENV == 'production';
 
-checkNodeV().then(() => {
+checkNodeV().then(function (){
+  var testTool = require('./test-tool/src');
+
   testTool.run(__dirname, isProduction).then(result => {
     if (result.passed) {
       process.exit(0);
     } else {
       process.exit(1);
     }
-  }).catch((error) => {
+  }).catch(function (error) {
     console.log(error);
     process.exit(1);
   });
