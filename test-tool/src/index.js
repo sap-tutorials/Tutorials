ef78845e97ad81635d8c88debb908f1facd20433
Yevheniia_Rakhmatova <yevheniia_rakhmatova@epam.com>
2018-12-18 15:49:14 +0200

    Exclude WIP{ from testing in last commit

--- test-tool/src/index.js
+++ test-tool/src/index.js
@@ -9,27 +9,39 @@ const { logTemplates } = require('./constants');
 globalChecker.initialise();
 
 const run = async (projectPath, isProdRunMode) => {
-    const interceptors = {
-        onAction: null,
-        onStart: null,
-    };
+  const interceptors = {
+    onAction: null,
+    onStart: null,
+  };
 
-    const { filePaths, withProgressTracker } = await consoleEntry.interact(projectPath);
-    const filteredPaths = filePaths.filter(filePath => path.basename(filePath).toLowerCase() !== 'readme.md');
-    if(withProgressTracker) {
-        interceptors.onStart = ({ actionsCount }) => {
-            const bar = new ProgressBar(logTemplates.progressBar, { total: actionsCount, width: 60 });
-            interceptors.onAction = () => bar.tick();
-        };
-    }
-    const checkResult = await globalChecker.check(filteredPaths, projectPath, isProdRunMode, interceptors);
-    const report = consoleReporter.generateReport(checkResult, filePaths.length);
-    consoleReporter.outputReportToConsole(report, filePaths.length);
-    return {
-        passed: checkResult.passed,
+  const { filePaths, withProgressTracker } = await consoleEntry.interact(projectPath);
+  const filteredPaths = filePaths.filter(filePath => path.basename(filePath).toLowerCase() !== 'readme.md');
+  if (withProgressTracker) {
+    interceptors.onStart = ({ actionsCount }) => {
+      const bar = new ProgressBar(logTemplates.progressBar, { total: actionsCount, width: 60 });
+      interceptors.onAction = () => bar.tick();
     };
+  }
+  const checkResult = await globalChecker.check(filteredPaths, projectPath, isProdRunMode, interceptors);
+  const report = consoleReporter.generateReport(checkResult, filePaths.length);
+  consoleReporter.outputReportToConsole(report, filePaths.length);
+  return {
+    passed: checkResult.passed,
+  };
+};
+
+const runSpecific = async (filePaths, projectPath, isProdRunMode, interceptors = {}) => {
+  await globalChecker.initialise();
+  const checkResult = await globalChecker.check(filePaths, projectPath, isProdRunMode, interceptors);
+  const report = consoleReporter.generateReport(checkResult, filePaths.length);
+
+  consoleReporter.outputReportToConsole(report, filePaths.length);
+  return {
+    passed: checkResult.passed,
+  };
 };
 
 module.exports = {
-    run,
+  run,
+  runSpecific,
 };
