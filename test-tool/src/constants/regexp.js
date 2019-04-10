d8219386c3a5db283fcc4a85743bbd557245e45d
Yevheniia Rakhmatova <yevheniia_rakhmatova@epam.com>
2019-04-10 08:03:08 +0100

    DEVMS-1890: Add validation of links that do not start with http(s)

--- test-tool/src/constants/regexp.js
+++ test-tool/src/constants/regexp.js
@@ -60,7 +60,7 @@ module.exports = {
         },
         localFileLink: {
             regexp: /\[[^\]]+\]\((?!http)([a-z\-_A-Z0-9]+?)\.[a-z]{2,10}\)/,
-            message: 'incorrect link to local file, use full link to file on GitHub',
+            message: 'Incorrect link to local file, use full link to file on GitHub (starting with https://)',
         },
         internalLink: {
             regexp: new RegExp('(sap\.corp)'),
