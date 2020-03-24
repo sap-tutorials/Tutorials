42018eb81d6ed0e4577c51d10941624c080b4b2b
Yevheniia_Rakhmatova <yevheniia_rakhmatova@epam.com>
2020-03-24 12:01:11 +0200

    DEVMS-2986: Refactor LinkChecker

--- test-tool/src/checkers/link-checker.js
+++ test-tool/src/checkers/link-checker.js
@@ -1,134 +1,76 @@
-const https = require('https');
-const { URL } = require('url');
-const fetch = require('node-fetch');
+const checkLinks = require('check-links');
+const { getStatusText, NOT_ACCEPTABLE } = require('http-status-codes');
 
 const { regexp: { content: { internalLink, remoteImage } }, linkCheck } = require('../constants');
 const { domains } = require('../../config/trusted.links.json');
-const { linkUtils } = require('../utils');
 
 process.env.UV_THREADPOOL_SIZE = linkCheck.UV_THREADPOOL_SIZE;
 
-const checkLinks = async (links, onCheck) => {
-  let processingLinks = links.map(link => checkLink(link));
-  if (onCheck) {
-    processingLinks = processingLinks.map(checkPromise => checkPromise.then(result => {
-      onCheck(result);
-      return result;
-    }));
-  }
-  const processedResults = await Promise.all(processingLinks);
-  const results = processedResults.filter(({ err, code }) => err || linkUtils.isErrorStatusCode(code)).map(({ link, code, err }) => {
-    const isTrusted = !!domains.find(domain => link.includes(domain));
-    return {
-      link,
-      code,
-      isTrusted,
-      reason: (err && (err.message || err)),
-    };
-  });
-  return results;
-};
+const isAlive = (status) => status === 'alive';
 
-const checkLink = async (link, reqOptions = {}, maxAttempts = 2) => {
-  try {
-    const { hostname } = new URL(link);
-
-    if (internalLink.regexp.test(hostname)) {
+const verifyLinks = async (links) => {
+  const processedResults = await checkLinks(links);
+  return Object
+    .entries(processedResults)
+    .filter(([link, { status }]) => !isAlive(status))
+    .map(([link, { statusCode }]) => {
+      const isTrusted = domains.some(domain => link.includes(domain));
+      const isInternal = internalLink.regexp.test(link);
       return {
         link,
-        code: 0,
-        err: internalLink.message,
+        isTrusted,
+        code: statusCode || 0,
+        reason: isInternal ? internalLink.message : getStatusText(statusCode),
       };
-    }
-  } catch (e) {
-    return {
-      link,
-      code: 0,
-      err: e.message,
-    };
-  }
-
-  return checkAttempt({ ...reqOptions, uri: link }, 1, maxAttempts);
-};
-
-const checkAttempt = async (options, attempt, maxAttempts) => {
-  const httpsAgent = new https.Agent({
-    rejectUnauthorized: false,
-  });
-
-  const fetchOptions = {
-    ...linkCheck.defaultClientOptions,
-    ...options,
-    agent: (linkUtils.isHttps(options.uri) ? httpsAgent : null),
-  };
-
-  try {
-    let response = await fetch(options.uri, { ...fetchOptions, method: 'head' });
-
-    if (!response.ok) {
-      response = await fetch(options.uri, { ...fetchOptions, method: 'get' });
-    }
-
-    return {
-      response,
-      link: options.uri,
-      code: response.status
-    };
-  } catch (error) {
-    if (error.message.startsWith('Protocol')) {
-      fetchOptions.agent = null;
-    }
-
-    try {
-      const response = await fetch(options.uri, { ...fetchOptions, method: 'get' });
-
-      return {
-        response,
-        link: options.uri,
-        code: response.status,
-      };
-    } catch (error) {
-      if (attempt <= maxAttempts) {
-        // some sites will work without user-agent header
-        return checkAttempt({ ...options, headers: {} }, ++attempt, maxAttempts);
-      } else {
-        return {
-          link: options.uri,
-          code: 0,
-          err: error.message,
-        };
-      }
-    }
-  }
+    });
 };
 
 const checkImageLink = async (link) => {
-  const result = await checkLink(link);
-  result.error = result.error || result.err;
+  const result = await checkLinks([link], {
+    hooks: {
+      afterResponse: [(response) => {
+        const contentType = (response.headers['Content-Type'] || '');
 
-  if (result.error) {
-    // ignore, in case of error link checker will throw it
-    delete result.error;
+        if (contentType.includes('image/')) {
+          return response;
+        }
+
+        response.statusCode = NOT_ACCEPTABLE;
+        return response;
+      }]
+    }
+  });
+  const linkResult = result[link];
+  const { status, statusCode } = linkResult;
+
+  if (`${statusCode}` === `${NOT_ACCEPTABLE}`) {
+    result.error = remoteImage.message;
     return result;
   }
 
-  if (result.response && !linkUtils.isErrorStatusCode(result.response.status)) {
-    const { response } = result;
-    const contentType = (response.headers.get('Content-Type') || '');
-
-    if (contentType.includes('image/')) {
-      return result;
-    } else {
-      result.error = remoteImage.message;
-    }
+  linkResult.error = isAlive(status) ? undefined : getStatusText(statusCode);
+  if (linkResult.error) {
+    // ignore, in case of error link checker will throw it
+    delete linkResult.error;
+    return linkResult;
   }
 
   return result;
 };
 
-module.exports = {
-  checkLinks,
-  checkLink,
-  checkImageLink,
+const checkLink = async (link) => {
+  const result = await checkLinks([link]);
+  const linkResult = result[link];
+  const { status, statusCode } = linkResult;
+  linkResult.error = isAlive(status) ? undefined : getStatusText(statusCode);
+  linkResult.code = statusCode;
+  linkResult.link = link;
+
+  return linkResult;
 };
 
+module.exports = {
+  checkLink,
+  checkImageLink,
+  checkLinks: verifyLinks,
+};
