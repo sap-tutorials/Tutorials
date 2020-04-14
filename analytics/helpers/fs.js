c80dd43e3e4d1741160a80b2b5e35a3ef93b2549
Yevheniia Rakhmatova <orristurmurminintur@gmail.com>
2020-04-14 14:15:53 +0300

    Analyze Memory Usage

--- analytics/helpers/fs.js
+++ analytics/helpers/fs.js
@@ -3,9 +3,21 @@
 const fs = require('fs');
 const util = require('util');
 
-module.exports = {
+const redefined = {
   access: util.promisify(fs.access.bind(fs)),
   writeFile: util.promisify(fs.writeFile.bind(fs)),
   readFile: util.promisify(fs.readFile.bind(fs)),
   readDir: util.promisify(fs.readdir.bind(fs)),
 };
+
+const proxy = new Proxy(fs, {
+  get(target, prop) {
+    if (prop in redefined) {
+      return redefined[prop];
+    } else if (prop in target) {
+      return target[prop];
+    }
+  }
+});
+
+module.exports = proxy;
