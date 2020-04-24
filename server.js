75546d9faa022de8857e270b841b0ec4895c7445
Yevheniia Rakhmatova <orristurmurminintur@gmail.com>
2020-04-24 13:34:12 +0300

    DEVMS-3097: Script to Create a Report with Tutorials and the Unused Images
    Remove unused file

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var server = http.createServer((function (request, response) {
-  response.writeHead(200,
-    { "Content-Type": "text/plain" });
-  response.end("Hello World\n");
-}));
-
-server.listen(8080);
